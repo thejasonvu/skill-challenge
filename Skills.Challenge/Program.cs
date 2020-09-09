07f9058bac0e0688e4ff4bc1236587469991f575
thejasonvu <jasonvietvu@gmail.com>
2020-09-09 15:34:02 -0400

    Sort fields in descending order

--- Skills.Challenge/Program.cs
+++ Skills.Challenge/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Text.Json;
+using System.Linq;
 
 namespace Skills.Challenge
 {
@@ -13,7 +14,14 @@ namespace Skills.Challenge
 
             var strBuilder = new StringBuilder();
 
-            foreach (var field in form.Fields)
+            // I use LINQ to sort the fields because form.Fields is IEnumerable which is read-only
+            // A way to sort without using LINQ is to instantiate a list and then sort on that but
+            // using LINQ is more succinct and semantic. Example:
+            // List<Field> listOfFields = form.Fields.ToList();
+            // listOfFields.Sort((x, y) => x.Order.CompareTo(y.Order));
+            var sortedFields = form.Fields.OrderByDescending(i => i.Order);
+
+            foreach (var field in sortedFields)
             {
                 strBuilder.AppendLine($"{field.Order}) {field.Name} = {field.Value}");
             }
