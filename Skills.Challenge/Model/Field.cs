0e777a38c196c5127878230c0350d860b8dc2989
thejasonvu <jasonvietvu@gmail.com>
2020-09-09 13:51:32 -0400

    Add Order property to Field Model

--- Skills.Challenge/Model/Field.cs
+++ Skills.Challenge/Model/Field.cs
@@ -4,11 +4,12 @@ using System.Text;
 
 namespace Skills.Challenge
 {
-	public class Field
-	{
-		public string Name { get; set; }
-		public string Type { get; set; }
-		public object Value { get; set; }
-		public bool Visible { get; set; }
-	}
+    public class Field
+    {
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public object Value { get; set; }
+        public bool Visible { get; set; }
+        public int Order { get; set; }
+    }
 }
